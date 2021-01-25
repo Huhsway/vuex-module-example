bfc2bc1441d6d9502fa7ebc9eae4530446b41fa6
Huhsway <seunghoe9542@naver.com>
2021-01-25 13:33:23 +0900

    수정

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      module1
+    </h1>
+    <h2>{{ module1ValueWithHello }}</h2>
+    <h3>{{ module1 }}</h3>
+    <br />
+
+    module1 값 변경
+    <input type="text" v-model="module1Value">
+    <button type="button" @click="changeModule1Value">변경</button>
+    <br />
+    <h1>
+      module2
+    </h1>
+    <h2>{{ module2ValueWithHello }}</h2>
+    <h3>{{ module2 }}</h3>
+    module2 값 변경
+    <input type="text" v-model="module2Value">
+    <button type="button" @click="changeModule2Value">변경</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapGetters } from 'vuex';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      module1Value: '',
+      module2Value: ''
+    };
+  },
+  methods: {
+    changeModule1Value() {
+      this.$store.dispatch('setModule1Value', this.module1Value);
+    },
+    changeModule2Value() {
+      this.$store.dispatch('setModule2Value', this.module2Value);
+    }
+  },
+  computed: {
+    ...mapState({
+      module1: state => state.module1.module1Value,
+      module2: state => state.module2.module2Value
+    }),
+    ...mapGetters({
+      module1ValueWithHello: 'module1ValueWithHello',
+      module2ValueWithHello: 'module2ValueWithHello'
+    })
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
